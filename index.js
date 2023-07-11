7b9f64d3d9d0e3b1c4b298620709de0d900c3fdd
Ravi515K <koliravi95@gmail.com>
2023-07-11 20:11:28 +0530

    Gallery- HTML,css and Js file

---
+++ index.js
@@ -0,0 +1,51 @@
+
+const ID= 'pcWSOlWA5LhSQD7u9NK8RHctqgSrX1TVnn2Zpf8-utQ'
+let val
+const handleChange=()=>{
+     val= document.querySelector("#input").value
+}
+
+
+const getImages=()=>{
+   // console.log(val)
+    
+   fetch(`https://api.unsplash.com/search/photos/?client_id=${ID}&query=${val}`)
+   .then((res)=>res.json())
+   .then((res)=>{
+    // console.log(res)
+        AppendData(res.results)
+
+   })
+}
+
+
+
+const handleSearch= () =>{
+     val= document.querySelector("#search").value;
+    getImages(val)
+
+}
+
+const AppendData = (data)=>{
+    console.log(data)
+    let main=document.querySelector('#grid')
+    main.innerHTML="";
+    data.map((el)=>{
+        let card=document.createElement('div');
+            card.className="card"
+        let img=document.createElement('Img');
+        img.src=el.urls.small
+
+        let name=document.createElement('h3');
+            name.innerHTML=el.user.name;
+
+        let description=   document.createElement('p'); 
+            description.innerHTML=el.description;
+
+        let link=document.createElement('a')
+            link.href=el.links.download
+            link.innerHTML="Original Picture Link"
+            card.append(img,name,description,link)
+            main.append(card)
+    })
+}
