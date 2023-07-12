71e3c2548862ecf486a0865a0ed0c424bdaa5af8
Ravi515K <koliravi95@gmail.com>
2023-07-12 14:59:15 +0530

    structured document

--- index.js
+++ index.js
@@ -1,57 +1,63 @@
 
-const ID= 'pcWSOlWA5LhSQD7u9NK8RHctqgSrX1TVnn2Zpf8-utQ'
-let val
-const handleChange=()=>{
-     val= document.querySelector("#input").value
+const ID = 'pcWSOlWA5LhSQD7u9NK8RHctqgSrX1TVnn2Zpf8-utQ'
+let val = null
+console.log(val)
+const handleChange = () => {
+    val = document.querySelector("#input").value
 }
 
 
-const getImages=()=>{
-   // console.log(val)
-    
-   fetch(`https://api.unsplash.com/search/photos/?client_id=${ID}&query=${val}`)
-   .then((res)=>res.json())
-   .then((res)=>{
-    // console.log(res)
-        AppendData(res.results)
+const getImages = () => {
 
-   })
+
+    fetch(`https://api.unsplash.com/search/photos/?client_id=${ID}&query=${val}`)
+        .then((res) => res.json())
+        .then((res) => {
+            // console.log(res)
+            AppendData(res.results)
+
+        })
+    let newVal = document.querySelector("#input")
+    newVal.innerHTML = ""
 }
 
 
 
-const handleSearch= () =>{
-     val= document.querySelector("#search").value;
-     fetch(`https://api.unsplash.com/search/photos/?client_id=${ID}&query=${val}`)
-     .then((res)=>res.json())
-     .then((res)=>{
-      // console.log(res)
-          AppendData(res.results)
-  
-     })
+const handleSearch = () => {
+    let x = document.querySelector("#search").value;
+    fetch(`https://api.unsplash.com/search/photos/?client_id=${ID}&query=${x}`)
+        .then((res) => res.json())
+        .then((res) => {
+            // console.log(res)
+            AppendData(res.results)
+
+        })
 
 }
 
-const AppendData = (data)=>{
+const AppendData = (data) => {
     console.log(data)
-    let main=document.querySelector('#grid')
-    main.innerHTML="";
-    data.map((el)=>{
-        let card=document.createElement('div');
-            card.className="card"
-        let img=document.createElement('Img');
-        img.src=el.urls.small
+    let main = document.querySelector('#grid')
+    main.innerHTML = "";
+    data.map((el) => {
+       
+        let card = document.createElement('div');
+        card.className = "card"
+       
+        let img = document.createElement('Img');
+        img.src = el.urls.small
 
-        let name=document.createElement('h3');
-            name.innerHTML=el.user.name;
+        let name = document.createElement('h3');
+        name.innerHTML = el.user.name;
 
-        let description=   document.createElement('p'); 
-            description.innerHTML=el.description;
+        let description = document.createElement('p');
+        description.innerHTML = el.description;
 
-        let link=document.createElement('a')
-            link.href=el.links.download
-            link.innerHTML="Original Picture Link"
-            card.append(img,name,description,link)
-            main.append(card)
+        let link = document.createElement('a')
+        link.href = el.links.download
+        link.innerHTML = "Original Picture Link"
+        
+        card.append(img, name, description, link)
+        main.append(card)
     })
 }
